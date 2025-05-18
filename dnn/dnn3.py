fae9e09854550222d4d1ffb88ddc4c5fdd2ccd31
Ezechungi7 <103564296+Ezechungi7@users.noreply.github.com>
2025-05-18 21:36:54 +0800

    run

--- dnn/dnn3.py
+++ dnn/dnn3.py
@@ -1,15 +1,18 @@
 from __future__ import print_function
-from sklearn.cross_validation import train_test_split
+#from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 np.random.seed(1337)  # for reproducibility
 from keras.preprocessing import sequence
-from keras.utils import np_utils
+#from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Embedding
 from keras.layers import LSTM, SimpleRNN, GRU
 from keras.datasets import imdb
-from keras.utils.np_utils import to_categorical
+from keras.utils import to_categorical
+#from keras.utils.np_utils import to_categorical
+#from tensorflow.keras.utils import to_categorical
 from sklearn.metrics import (precision_score, recall_score,f1_score, accuracy_score,mean_squared_error,mean_absolute_error)
 from sklearn import metrics
 from sklearn.preprocessing import Normalizer
@@ -17,8 +20,8 @@ import h5py
 from keras import callbacks
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, CSVLogger
 
-traindata = pd.read_csv('kdd/binary/Training.csv', header=None)
-testdata = pd.read_csv('kdd/binary/Testing.csv', header=None)
+traindata = pd.read_csv('dnn/kdd/binary/Training.csv', header=None)
+testdata = pd.read_csv('dnn/kdd/binary/Testing.csv', header=None)
 
 X = traindata.iloc[:,1:42]
 Y = traindata.iloc[:,0]
@@ -54,10 +57,10 @@ model.add(Activation('sigmoid'))
 
 # try using different optimizers and different optimizer configs
 model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
-checkpointer = callbacks.ModelCheckpoint(filepath="kddresults/dnn3layer/checkpoint-{epoch:02d}.hdf5", verbose=1, save_best_only=True, monitor='loss')
+checkpointer = callbacks.ModelCheckpoint(filepath="kddresults/dnn3layer/checkpoint-{epoch:02d}.keras", verbose=1, save_best_only=True, monitor='loss')
 csv_logger = CSVLogger('kddresults/dnn3layer/training_set_dnnanalysis.csv',separator=',', append=False)
-model.fit(X_train, y_train,  batch_size=batch_size, nb_epoch=100, callbacks=[checkpointer,csv_logger])
-model.save("kddresults/dnn3layer/dnn3layer_model.hdf5")
+model.fit(X_train, y_train,  batch_size=batch_size, epochs=100, callbacks=[checkpointer,csv_logger])
+model.save("dnn/chung_results/dnn3layer_model.hdf5")
 
 
 
