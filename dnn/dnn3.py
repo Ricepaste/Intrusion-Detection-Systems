f2f6bff17704898a826512a02b435b2e9dec292a
Paul Wang <paul33931029@gmail.com>
2025-06-12 15:55:49 +0800

    Update dnn3.py

--- dnn/dnn3.py
+++ dnn/dnn3.py
@@ -22,7 +22,35 @@ testdata = pd.read_csv('kdd/binary/Testing.csv', header=None)
 
 X = traindata.iloc[:,1:42]
 Y = traindata.iloc[:,0]
-	@@ -54,15 +57,7 @@
+C = testdata.iloc[:,0]
+T = testdata.iloc[:,1:42]
+
+scaler = Normalizer().fit(X)
+trainX = scaler.transform(X)
+
+scaler = Normalizer().fit(T)
+testT = scaler.transform(T)
+
+y_train = np.array(Y)
+y_test = np.array(C)
+
+
+X_train = np.array(trainX)
+X_test = np.array(testT)
+
+
+batch_size = 64
+
+# 1. define the network
+model = Sequential()
+model.add(Dense(1024,input_dim=41,activation='relu'))  
+model.add(Dropout(0.01))
+model.add(Dense(768,activation='relu'))  
+model.add(Dropout(0.01))
+model.add(Dense(512,activation='relu'))  
+model.add(Dropout(0.01))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
 
 # try using different optimizers and different optimizer configs
 model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
